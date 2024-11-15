bbd947ea892672c2cb6a3ef14294f34d1816857a
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-15 07:43:24 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,16 @@ url_list_maker = st.Page(page="crawl_web_pages.py", title="URL List Maker", icon
 class_name_finder = st.Page(page="search_keyword.py", title="Class Name Finder", icon=":material/search:")
 summarizer = st.Page(page="summarize_to_doc.py", title="Summarizer", icon=":material/apps:")
 pg = st.navigation([url_list_maker, class_name_finder, summarizer])
-pg.run()
 
-# Streamlitアプリのタイトルを設定
 st.title("Placeholder for toppage")
 
 st.markdown('---')
 st.markdown('description')
 st.markdown('---')
+
+
+
+pg.run()
+
+# Streamlitアプリのタイトルを設定
+
