76aa70230eb4f5b77519cff9fab8b22dc776b897
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-15 07:59:24 +0000

    aaa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,2 @@
 import streamlit as st
 
-st.set_page_config(page_title="Streamlit App", page_icon=":shark:")
-url_list_maker = st.Page(page="/contents/01_crawl_web_pages.py", title="URL List Maker")
-class_name_finder = st.Page(page="/contents/02_search_keyword_in_div.py", title="Class Name Finder")
-summarizer = st.Page(page="/contents/03_summarize_to_doc.py", title="Summarizer")
-pg = st.navigation([url_list_maker, class_name_finder, summarizer])
-pg.run()
