21d0d13f0bf6836777c0dcaba419003908cd994d
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-19 03:31:05 +0000

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+st.title("使い方")
+st.write("""
+URL List Maker: URLのページに記載されているリンクを再帰的に辿ってURLのリストを作成します。
+Class Name Finder: URLを指定したページをキーワード検索し、該当箇所のClass Nameを表示します。
+Summarizer: URLのリスト(txtファイル)に順番にアクセスし、指定したClass Nameの箇所を抜粋してGeminiで要約した結果をテキストファイルにまとめます。
 
-st.write("Hello World!")
+""")
