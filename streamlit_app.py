e07a9d54e99f2b3c329c9c44ef2014abae87ecc8
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-19 03:54:22 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 st.title("使い方 📖")
 st.markdown("""
-### URL List Maker: 
+---
+### URL List Maker
 URLのページに記載されているリンクを再帰的に辿ってURLのリストを作成します。
-### Class Name Finder:
+### Class Name Finder
 URLを指定したページをキーワード検索し、該当箇所のClass Nameを表示します。
-### Summarizer:
+### Summarizer
 URLのリスト(txtファイル)に順番にアクセスし、指定したClass Nameの箇所を抜粋してGeminiで要約した結果をテキストファイルにまとめます。
 
 """)
