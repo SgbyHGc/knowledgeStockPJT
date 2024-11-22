a92146641926c7e4fa42a99e2d5dead5e2ac3856
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-23 00:40:11 +0900

    １

--- pages/01_URL_List_Maker.py
+++ pages/01_URL_List_Maker.py
@@ -78,10 +78,9 @@ start_url = st.text_input('URLを入力してください', value='https://www.t
 url_pattern = st.text_input('キーワードを入力してください', value='/marketing-strategies/')
 max_depth = st.number_input('最大深度を入力してください', min_value=1, max_value=3, value=2)
 
-# if st.button("Search"):
-    # urls = crawl_web_pages(start_url, url_pattern, max_depth)
-urls = ["aaa","bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"]
-if urls:
+if st.button("Search"):
+    urls = crawl_web_pages(start_url, url_pattern, max_depth)
+    # urls = ["aaa","bbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"]
     selected_urls = []
     with st.form('my form'):
         for url in urls:
