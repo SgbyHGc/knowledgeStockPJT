239cb0f6a6cb1e93df3aee893b07cc1182365f78
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-22 08:32:26 +0000

    1

--- pages/01_URL_List_Maker.py
+++ pages/01_URL_List_Maker.py
@@ -119,20 +119,20 @@ if st.button("Search"):
   with st.spinner('Crawling... This may take minutes'):
       urls = crawl_web_pages(start_url, url_pattern, max_depth)
       
-if urls:
-  st.subheader('results:')
-  for url in urls:
-    checked_urls[url] = st.checkbox(url)
-  if checked_urls:
-    selected_urls = [url for url, checked in checked_urls.items() if checked]
-  if selected_urls:
-    txt_data = "\n".join(selected_urls)
-    st.download_button(
-        label="Download txt file",
-        data=txt_data,
-        file_name="urls.txt",
-        mime="text/plain",
-    )
+      if urls:
+        st.subheader('results:')
+        for url in urls:
+          checked_urls[url] = st.checkbox(url)
+        if checked_urls:
+          selected_urls = [url for url, checked in checked_urls.items() if checked]
+        if selected_urls:
+          txt_data = "\n".join(selected_urls)
+          st.download_button(
+              label="Download txt file",
+              data=txt_data,
+              file_name="urls.txt",
+              mime="text/plain",
+          )
 
 
 
