425532a5b449fdfd02d787a8d3e0dd4a3b8d84c9
op07cp <90225142+op07cp@users.noreply.github.com>
2024-11-23 13:12:26 +0900

    1

--- pages/01_URL_List_Maker.py
+++ pages/01_URL_List_Maker.py
@@ -102,16 +102,15 @@ URLのページに記載されているリンクを辿ってURLのリストを�
 深度は、リンク先のリンクの深さを示します。リンク先のリンク先のリンクまで収集する場合は3。
 """)
 st.markdown('---')
-
+start_url = st.text_input('URLを入力してください', value='https://www.thinkwithgoogle.com/intl/ja-jp/marketing-strategies/')
+url_pattern = st.text_input('キーワードを入力してください', value='/marketing-strategies/')
+max_depth = st.number_input('最大深度を入力してください', min_value=1, max_value=3, value=2)
+st.markdown('---')
 
 if 'urls' not in st.session_state:
     st.session_state.urls = []
 if 'selected_urls' not in st.session_state:
     st.session_state.selected_urls = []
-    
-start_url = st.text_input('URLを入力してください', value='https://www.thinkwithgoogle.com/intl/ja-jp/marketing-strategies/')
-url_pattern = st.text_input('キーワードを入力してください', value='/marketing-strategies/')
-max_depth = st.number_input('最大深度を入力してください', min_value=1, max_value=3, value=2)
 
 if st.button('crawl'):
     urls = crawl_web_pages(start_url, url_pattern, max_depth)
